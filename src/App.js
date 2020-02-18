fa16a9b4ccb53c0e92db867d8991eec0b8ef1f78
Julie <graffgj@hotmail.fr>
2020-02-18 17:01:23 +0100

    feat(weather):add icon, description, temperature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import Weather from './components/Weather';
+import Weather from './components/weather';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <Weather/>
+        <Weather />
       </header>
+      
     </div>
   );
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { mockWeather } from '../mork/mockWeather';
 
 
-function weather() {
+function Weather() {
+
+    const [weather, setweather] = useState(mockWeather);
+
+    function KelvinToCelsius(tempKelvin){
+        return Math.round(tempKelvin - 273.15);
+    }
+
+    function loadIconWeather(idIcon){
+        return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png"
+    }
     return (
         <div>
-            <Météo/>
+            <h1>Météo : {weather.name}</h1> 
+            <img alt ="" src={loadIconWeather(weather.weather[0].icon)}></img>
+            <p>{weather.weather[0].description}</p>
+            <p>{KelvinToCelsius(weather.main.temp)} °C</p>
         </div>
     )
 }
 
-export default weather
+export default Weather
