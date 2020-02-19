dd8e0a636cd0b2bc204cb0f68a817835828f6349
Julie <graffgj@hotmail.fr>
2020-02-19 10:55:38 +0100

    weather data

--- src/action/weatherAction.js
+++ src/action/weatherAction.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 // Requete GET 
 
+const baseUrl = "http://api.openweathermap.org/data/2.5";
+const appId ="&appid={f6939eb98229a91f3410b12041f0295d}"
 
 export function getWeather(){
-    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=London");
+    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=Essey-les-nancy");
 }
