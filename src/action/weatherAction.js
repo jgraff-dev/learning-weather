97b931b8987254edf62ad31fad9c4978daf0c7a2
Julie <graffgj@hotmail.fr>
2020-02-19 15:47:20 +0100

    last chances

--- src/action/weatherAction.js
+++ src/action/weatherAction.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 // Requete GET 
 
 const baseUrl = "http://api.openweathermap.org/data/2.5";
-const appId ="&appid={f6939eb98229a91f3410b12041f0295d}"
+const appId = "&appid=f6939eb98229a91f3410b12041f0295d";
 
-export function getWeather(){
-    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=Essey-les-nancy");
+export function getWeather(ville){
+    return axios.get(baseUrl+"/weather?q="+ville+appId);
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -6,17 +6,18 @@ import { mockWeather } from '../mork/mockWeather';
 function Weather() {
 
     const [weather, setweather] = useState(mockWeather);
+    const [ville, setville] = useState("Nancy");
 
     useEffect(() => {
         loadWeatherData();
-    }, [])
+    }, [ville])
 
     function KelvinToCelsius(tempKelvin){
         return Math.round(tempKelvin - 273.15);
     }
 
     async function loadWeatherData(){
-        const weatherAjax = await getWeather();
+        const weatherAjax = await getWeather(ville);
         console.log(weatherAjax);
         setweather(weatherAjax.data)
     }
@@ -27,17 +28,21 @@ function Weather() {
     }
     return (
         <div>
-            { weather ?
-                <div>
-                    <h1>Météo : {weather.name}</h1> 
-                    <img alt ="" src={loadIconWeather(weather.weather[0].icon)}></img>
-                    <p>{weather.weather[0].description}</p>
-                    <p>{KelvinToCelsius(weather.main.temp)} °C</p>
-            </div>
-        : <div>
-        <h1>Météo en attente de chargement</h1>
-        </div>
-        }
+            <input type="text" value={ville} onChange={ville} />
+                { weather ?
+                    <div>
+                        <h1>Météo : {weather.name}</h1> 
+                        <img alt ="" src={loadIconWeather(weather.weather[0].icon)}></img>
+                        <p>{weather.weather[0].description}</p>
+                        <p>{KelvinToCelsius(weather.main.temp)} °C</p>
+                        <p>Humidité dans l'air : {weather.main.humidity} %</p>
+                        <p>Vitesse du vent : {weather.wind.speed}</p>
+                        <p>Température ressentis : {KelvinToCelsius(weather.main.feels_like)} °C</p>
+                    </div>
+                    : <div>
+                        <h1>Météo en attente de chargement</h1>
+                    </div>
+                    }
         </div>
     )
 }
